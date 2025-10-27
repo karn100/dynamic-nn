fbdbc7f398a8fb5ce007329200f1dbcb7ab85d36
karn100 <karansharma020710@gmail.com>
2025-10-27 23:03:55 +0530

    added and modified

---
+++ tests/test_module.py
@@ -0,0 +1,15 @@
+import torch
+from core_mlp.module import Module,Parameter
+
+def test_module_registration():
+    class Dummy(Module):
+        def __init__(self):
+            super().__init__()
+            self.w = Parameter(torch.randn(3,3))
+            self.b = Parameter(torch.randn(3))
+        
+        def forward(self, x):
+            return x@self.w + self.b
+    
+    model = Dummy()
+    assert(len(list(model.parameters()))) == 2
