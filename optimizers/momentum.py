5fda2474d0c441895e63e3418e8b567fc0c540bb
karn100 <karansharma020710@gmail.com>
2025-10-31 13:11:40 +0530

    added NAG and made new file tensor.py

--- optimizers/momentum.py
+++ optimizers/momentum.py
@@ -18,5 +18,30 @@ class Momentum(Optimizer):
             grad = p.grad
 
             v.mul_(self.momentum).add_(grad,alpha=(1 - self.momentum))
-            p.data.add_(v,alpha=self.lr)
+            p.data.add_(v,alpha=-self.lr)
+
+
+class MomentumwithNAG(Optimizer):
+    def __init__(self, params,lr: float = 0.1,momentum: float = 0.9):
+        super().__init__(params)
+        self.lr = lr
+        self.momentum = momentum
+
+        # set initial velcities of paramteres(weights,bias) to 0 with same size as theirs in dict.
+        self.velocities = {p: torch.zeros_like(p) for p in self.params}
+
+    def step(self):
+        for p in self.params:
+            if p.grad is None:
+                continue
+
+            v = self.velocities[p]
+            grad = p.grad
+
+            v_prev = v.clone()
+
+            v.mul_(self.momentum).add_(grad)
+
+            p.data.add_(v.mul(self.momentum) + grad,alpha=-self.lr)
+            # self.velocities[p] = v_prev.mul(self.momentum).add_(grad)
             
--- tests/test_optimizers.py
+++ tests/test_optimizers.py
@@ -1,13 +1,14 @@
 import torch
 from core_mlp.module import Parameter
 from optimizers.SGD import SGD
+from optimizers.momentum import Momentum,MomentumwithNAG
 
 w = Parameter(torch.randn(()),require_grad=True)
-optimizer = SGD([w],lr=0.1)
+optimizer = MomentumwithNAG([w],lr=0.1,momentum=0.9)
 
 for step in range(100):
 
-    y_pred = w * 3
+    y_pred = w * 1
     loss = (y_pred - 2)**2
 
     loss.backward()
