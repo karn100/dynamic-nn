11028e2dd1a1d7bd968d8b60883ee12b9a9528d4
karn100 <karansharma020710@gmail.com>
2025-11-07 15:28:18 +0530

    added adagrad and rmsprop optimizers

--- optimizers/adam.py
+++ optimizers/adam.py
@@ -3,5 +3,46 @@ import torch
 from optimizers.optim import Optimizer
 
 class AdaGrad(Optimizer):
-    def __init__(self, params):
-        super().__init__(params)
+    def __init__(self, params,lr=0.01,eps=1e-8):
+        super().__init__(params,lr)
+        self.eps = eps
+
+        self.state = {
+            id(p): {"G" : torch.zeros_like(p.data)}
+            for group in self.params_group
+            for p in group["params"]
+            }
+    
+    def step(self):
+        for group in self.params_group:
+            lr = group["lr"]
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                state = self.state[id(p)]
+                G = state["G"]
+
+                G.addcmul_(p.grad,p.grad,value=1)
+                p.data.add_(-lr * p.grad / (G.sqrt() + self.eps))
+
+class RMSProp(Optimizer):
+    def __init__(self, params, lr= 0.01, alpha = 0.9,eps = 1e-8):
+        super().__init__(params, lr)
+        self.eps = eps
+        self.alpha = alpha
+
+        self.state = {id(p): {"E": torch.zeros_like(p.data)}
+                      for group in self.params_group
+                      for p in group["params"]}
+        
+    def step(self):
+        for group in self.params_group:
+            lr = group["lr"]
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                state = self.state[id(p)]
+                E = state["E"]
+
+                E.mul_(self.alpha).addcmul_(p.grad,p.grad,value=(1 - self.alpha))
+                p.data.add_(-lr * p.grad / E.sqrt() + self.eps)
--- tests/test_optimizers.py
+++ tests/test_optimizers.py
@@ -2,13 +2,14 @@ import torch
 from core_mlp.module import Parameter
 from optimizers.SGD import SGD
 from optimizers.momentum import Momentum,MomentumwithNAG
+from optimizers.adam import AdaGrad,RMSProp
 
 w = Parameter(torch.randn(()),require_grad=True)
-optimizer = MomentumwithNAG([w],lr=0.1,momentum=0.9)
+optimizer = RMSProp([w],lr=0.1,alpha=0.9,eps=1e-8)
 
 for step in range(100):
 
-    y_pred = w * 3
+    y_pred = w * 1
     loss = (y_pred - 2)**2
 
     loss.backward()
