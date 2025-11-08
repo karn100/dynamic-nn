7d65618c6fd5b502a9901f0f3663dd034401bb5b
karn100 <karansharma020710@gmail.com>
2025-11-08 14:59:54 +0530

    added adam

--- optimizers/adam.py
+++ optimizers/adam.py
@@ -46,3 +46,38 @@ class RMSProp(Optimizer):
 
                 E.mul_(self.alpha).addcmul_(p.grad,p.grad,value=(1 - self.alpha))
                 p.data.add_(-lr * p.grad / E.sqrt() + self.eps)
+
+class Adam(Optimizer):
+    def __init__(self, params, lr = 1e-3,beta1 = 0.9,beta2 = 0.999,eps = 1e-8):
+        super().__init__(params, lr)
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.eps = eps
+
+        self.state = {id(p): {"m":torch.zeros_like(p.data),"v": torch.zeros_like(p.data),"t":0}
+                      for group in self.params_group
+                      for p in group["params"]}
+    
+    def step(self):
+        for group in self.params_group:
+            lr = group["lr"]
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                
+                g = p.grad
+                state = self.state[id(p)]
+
+                v = state["v"]
+                m = state["m"]
+
+                state["t"] += 1
+                t = state["t"]
+
+                m.mul_(self.beta1).add_(g, alpha = (1 - self.beta1))
+                v.mul_(self.beta2).addcmul_(g,g, value = (1 - self.beta2))
+
+                m_hat = m / (1 - self.beta1**t)
+                v_hat = v / (1 - self.beta2**t)
+
+                p.data.add_(-lr * m_hat / (torch.sqrt(v_hat) + self.eps))
--- tests/test_optimizers.py
+++ tests/test_optimizers.py
@@ -2,14 +2,14 @@ import torch
 from core_mlp.module import Parameter
 from optimizers.SGD import SGD
 from optimizers.momentum import Momentum,MomentumwithNAG
-from optimizers.adam import AdaGrad,RMSProp
+from optimizers.adam import AdaGrad,RMSProp,Adam
 
 w = Parameter(torch.randn(()),require_grad=True)
-optimizer = RMSProp([w],lr=0.1,alpha=0.9,eps=1e-8)
+optimizer = Adam([w],lr=0.1,beta1=0.9,beta2=0.99,eps=1e-8)  #adjusting lr according to params and optimizer is very important
 
 for step in range(100):
 
-    y_pred = w * 1
+    y_pred = w * 2
     loss = (y_pred - 2)**2
 
     loss.backward()
